52c3f7065d69c56be96e5377ac45b9b83babf962
Anmol Dwivedi <193028208+anmolldwivedii@users.noreply.github.com>
2024-12-28 10:59:50 +0530

    Created small calculator.cpp

---
+++ small calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout << "Enter num1 : ";
+    cin >> num1;
+    cout << "Enter num2 : ";
+    cin >> num2;
+    
+    cout << "Sum is = " << (num1 + num2) << endl;
+    cout << "difference is = " << (num1 - num2) << endl;
+    cout << "product is = " << (num1 * num2) << endl;
+    cout << "quotient is = " << (num1 / num2) << endl;
+    cout << "modulo is = " << (num1 % num2) << endl;
+    
+    return 0;
+}
