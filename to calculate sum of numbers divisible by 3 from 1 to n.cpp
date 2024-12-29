7cfac884cc5774ef3045f5c518ae37bb0e916513
Anmol Dwivedi <193028208+anmolldwivedii@users.noreply.github.com>
2024-12-29 16:01:16 +0530

    Add to calculate sum of numbers divisible by 3 from 1 to n.cpp

---
+++ to calculate sum of numbers divisible by 3 from 1 to n.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num = 10, sum = 0;
+    for (int i = 1; i <= num; i++) {
+        if (i % 3 == 0) {
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
