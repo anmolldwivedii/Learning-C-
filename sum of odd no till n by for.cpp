7e4207258fdc5d7224a3a37606eaf9cecfe01e8b
Anmol Dwivedi <193028208+anmolldwivedii@users.noreply.github.com>
2024-12-29 16:18:57 +0530

    Create sum of odd no till n by for.cpp

---
+++ sum of odd no till n by for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0, num;
+    cout << "Enter the value of num : ";
+    cin >> num;
+    
+    for (int i = 1; i <= num; i += 2) {
+        sum += i;
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
