c336d88f12cf121b8527710a4ded7720695286e5
Anmol Dwivedi <193028208+anmolldwivedii@users.noreply.github.com>
2024-12-29 16:10:24 +0530

    Create calculating prime not efficient method.cpp

---
+++ calculating prime not efficient method.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    bool isprime = true;
+    cout << "Enter the value of num : ";
+    cin >> num;
+    
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            isprime = false;
+            break;
+        }
+    }
+    cout << (isprime == 1 ? "Is prime" : "Not prime");
+    
+    return 0;
+}
