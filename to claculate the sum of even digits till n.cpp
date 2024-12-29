567ecb04f1107f0ebf07ef893f596566f18f8dc6
Anmol Dwivedi <193028208+anmolldwivedii@users.noreply.github.com>
2024-12-29 16:25:37 +0530

    Create to claculate the sum of even digits till n.cpp

---
+++ to claculate the sum of even digits till n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0, num;
+    cout << "Enter the value of num : ";
+    cin >> num;
+    
+    for (int i = 1; i <= num; i++) {
+        if (i%2 == 0) {
+            sum += i;
+        }
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
